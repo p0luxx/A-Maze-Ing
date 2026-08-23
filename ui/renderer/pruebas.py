fe731e61220a040b69f3a7e34d0b6e6a233568f2
oscar_42 <suarezgonzalezoscar36@gmail.com>
2026-08-23 22:02:12 +0200

    vamos seguimos poco a poco

--- ui/renderer/pruebas.py
+++ ui/renderer/pruebas.py
@@ -1,15 +1,27 @@
-from mazegen.grid import Grid, Walls
+
 
 def ft_printeando():
     print("+---+")
     print("|   |")
     print("+---+")
 
-
-Matriz = Grid(2,2)
-print(Matriz)
-for fila in Matriz.grid:
-    for cell in fila:
-        if cell.lista == 15:
-            ft_printeando()
-
+for i in range(3):
+    print("*", end="")
+    c = 0
+    while (c <= 2):
+        if c == 1:
+            print("    ", end="")
+        else:
+            print("----", end="")
+        print("*", end="")
+        c += 1
+    print()
+    print("|", end="")
+    for _ in range(3):
+        print("    |", end="")
+    print()
+    if i == 2: #es decir si ha llegado al final, ponemos una de mas
+        print("*", end="")
+        for _ in range(3):
+            print("----*", end="")
+        
