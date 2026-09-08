f7444730fd164f7cf013e2a036449fd868dc39bd
oscar_42 <suarezgonzalezoscar36@gmail.com>
2026-09-08 15:41:46 +0200

    creamos la funcion change colors

--- ui/renderer/base.py
+++ ui/renderer/base.py
@@ -5,6 +5,13 @@
 from abc import ABC, abstractmethod
 from mazegen.grid import Grid, Walls
 from mazegen.strategies.base import GenerationStrategy
+from enum import Enum
+
+class WallColor(str, Enum):
+    CYAN = "\033[36m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
 
 
 class Renderer(ABC):
@@ -14,6 +21,7 @@ class Renderer(ABC):
         self.matriz: Grid = grid
         self.entry: tuple[int, int] = entry
         self.exit: tuple[int, int] = exit_
+        self.wall_color: WallColor = WallColor.CYAN
     
     def clear_screen(self):
         os.system("cls" if os.name == "nt" else "clear")
@@ -40,3 +48,9 @@ class Renderer(ABC):
             generator: yields a new coordinate step by step
             delay: slows the animation of the maze
         """
+
+    def ChangeColor(self) -> str:
+        colors: list[WallColor] = [WallColor]
+        current = colors.index[self.wall_colors]
+        next_color = (current + 1) % len(colors)
+        self.wall_colors = colors[next_color]
