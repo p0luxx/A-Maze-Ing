d96dc504a2e9d86581eafb917737b54ffba0197f
oscar_42 <suarezgonzalezoscar36@gmail.com>
2026-09-12 13:31:11 +0200

    salinedo cositas del prim

--- mazegen/strategies/backtracker.py
+++ mazegen/strategies/backtracker.py
@@ -45,9 +45,79 @@ class IterativeBacktrackerStrategy(GenerationStrategy):
                     yield stack[-1]
 
 
+class RandomIterativeBacktrackerStrategy(GenerationStrategy):
+    def generate(
+        self, grid: Grid, seed: int, start_gen: tuple[int, int]
+    ) -> Generator[tuple[int, int], None, None]:
+        random.seed(seed)
+        stack: list[tuple[int, int]] = [start_gen]
+        visited: set[tuple[int, int]] = {start_gen}
+        yield start_gen
+        while stack:
+            current = random.choice(stack) #cambio
+            cx, cy = current
+            unvisited_neighbours = [
+                n for n in grid.neighbours(current)
+                if n not in visited and not grid[n].blocked
+            ]
+
+            if unvisited_neighbours:
+                nx, ny = random.choice(unvisited_neighbours)
+                if ny < cy:
+                    grid[cx, cy].lista &= ~Walls.norte
+                    grid[nx, ny].lista &= ~Walls.sur
+                elif ny > cy:
+                    grid[cx, cy].lista &= ~Walls.sur
+                    grid[nx, ny].lista &= ~Walls.norte
+                elif nx > cx:
+                    grid[cx, cy].lista &= ~Walls.este
+                    grid[nx, ny].lista &= ~Walls.oeste
+                elif nx < cx:
+                    grid[cx, cy].lista &= ~Walls.oeste
+                    grid[nx, ny].lista &= ~Walls.este
+                visited.add((nx, ny))
+                stack.append((nx, ny))
+                yield (nx, ny)
+            else:
+                stack.pop()
+                if stack:
+                    yield stack[-1]
+
+
 class Prim(GenerationStrategy):
     def generate(
             self, grid: Grid, seed: int, start_gen: tuple[int, int]
             ) -> Generator[tuple[int, int], None, None]
         random.seed(seed)
+        visited: set[tuple[int, int]] = {start_gen}
+        active_candidates: list[tuple[int, int]] = [start_gen]
+        available_neighbours: list[tuple[int, int]] = []
+        yield start_gen
+        while active_candidates:
+            current = active_candidates[-1]
+            available_neighbours.extend(
+                n for n in grid.neighbours(current)
+                if n not in visited and not grid[n].blocked)
+            if available_neighbours:
+                x, y = random.choice(available_neighbours)
+                if (x, y - 1) in visited:
+                    grid[x, y -1].lista &= ~Walls.sur
+                    grid[x, y].lista &= ~Walls.norte
+                elif (x, y + 1) in visited:
+                    grid[x, y].lista &= ~Walls.sur
+                    grid[x, y + 1].lista &= ~Walls.norte
+                elif (x - 1, y) in visited:
+                    grid[x - 1, y].lista &= ~Walls.este
+                    grid[x, y].lista &= ~Walls.oeste
+                elif (x + 1, y) in visited:
+                    grid[x, y].lista &= ~Walls.este
+                    grid[x + 1, y].lista &= ~Walls.oeste
+            #continuarra!
+
+                    
+                    
+                
+            
+            else:
+                active_candidates.pop()
             
